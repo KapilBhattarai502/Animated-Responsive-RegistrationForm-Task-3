7c308515ae34ace19f9978e7c263c441bb503f70
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-05-10 21:34:29 +0545

    Sucessfully Completed

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 import express from "express";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 import path from "path";
-import { dirname } from 'path';
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-app.get("/",(req,res)=>{
-    res.sendFile("public/index.html",{root:__dirname});
-})
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
 
-//routing 
-app.post("/submit",(req,res)=>{
-    console.log(req.body);
-    return res.status(200).send(`${req.body.FirstName} your form is submitted Sucessfully`); 
-})
-//creating a server 
+//routing
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  return res
+    .status(200)
+    .send(`${req.body.FirstName} your form is submitted Sucessfully`);
+});
+//creating a server
 const port = 8000;
 
 app.listen(port, () => {
